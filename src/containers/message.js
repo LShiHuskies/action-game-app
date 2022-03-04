cf39ee41949f78e82aec69cc87af19cf2e4cc51a
Louis Shi <Shilou2010@gmail.com>
2022-03-03 22:34:33 -0500

    redirect to root for message when no message

--- src/containers/message.js
+++ src/containers/message.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
+import { useHistory } from "react-router-dom";
 
 
 
-const messageComp = ({ message }) => {
+const MessageComp = ({ message }) => {
+    let history = useHistory();
+
+    if (!message) {
+        history.push('/');
+    }
 
     return <div style={{ height: '100%', width: '100%', backgroundColor: 'rgba(128,128, 128,1)', position: 'absolute' }}>
         <Stack justifyContent="center" alignItems="center" spacing={2} style={{ marginTop: '20%' }}>
@@ -21,4 +27,4 @@ const mapStatetoProps = (state) => {
 }
 
 
-export default connect(mapStatetoProps)(messageComp);
+export default connect(mapStatetoProps)(MessageComp);
